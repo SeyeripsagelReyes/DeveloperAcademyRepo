77d75652214054e9ce8d7e69ec731f8a17c61b7e
Seyer <seyer.reyes@norimaconsulting.com>
2022-05-03 12:48:05 +0800

    commit12

---
+++ Chapter9/Exercise1/UEmployee.java
@@ -0,0 +1,15 @@
+class UEmployee {
+    public static void main(String[] args) {
+        UEmployee employee =new UEmployee();
+        UEmployee faculty = new Faculty();
+        UEmployee stuff = new Staff();
+
+    
+    }
+
+
+    public void show(){
+        System.out.println("Eplyi");
+    }
+    
+}
